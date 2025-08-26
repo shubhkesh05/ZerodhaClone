cb6092ce330be11ab0b8def03efa220ad501e522
shubhkesh05 <kesharwanishubh17@gmail.com>
2025-08-26 23:54:07 +0530

    Update PricingPage.js

--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "./Hero";
 import Brokerage from "./Brokerage";
 import OpenAccount from "../OpenAccount";
-
+export const url  = 'https://zerodha-backend-ko6u.onrender.com';
 function PricingPage() {
   return (
     <>
