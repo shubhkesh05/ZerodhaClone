3145dee1ca58b51ef0e329557cefe0eb77e2039f
shubhkesh05 <kesharwanishubh17@gmail.com>
2025-08-27 00:00:05 +0530

    Update Home.js

--- dashboard/src/components/Home.js
+++ dashboard/src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
+export const url  = 'https://zerodha-backend-ko6u.onrender.com';
 
 const Home = () => {
   return (
