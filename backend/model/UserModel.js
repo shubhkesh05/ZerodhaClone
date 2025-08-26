dd843386ff227505baa4688d85abb529ab7ab588
shubhkesh05 <kesharwanishubh17@gmail.com>
2025-08-26 21:50:31 +0530

    Delete backend directory

--- backend/model/UserModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const { UserSchema } = require('../schemas/UserSchema');
-
-const UserModel = mongoose.model('User', UserSchema);
-
-module.exports = { UserModel };
