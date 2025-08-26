beefc0d10342f0f55ce6464a75fe77dd7f50a948
shubhkesh05 <kesharwanishubh17@gmail.com>
2025-08-26 21:51:04 +0530

    Delete frontend directory

--- frontend/src/components/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import DashboardApp from './DashboardApp';
-import './Dashboard.css';
-
-const Dashboard = () => {
-  return <DashboardApp />;
-};
-
-export default Dashboard;
